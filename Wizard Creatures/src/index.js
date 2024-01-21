b000bfb3b96e6f46a4ac4e228740ea80084b739f
Daniel Boranski <dboranski@gmail.com>
2024-01-21 21:53:24 +0200

    connect to database via mongoose

--- Wizard Creatures/src/index.js
+++ Wizard Creatures/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const path = require("path");
+const mongoose = require("mongoose");
 
-const { PORT } = require("./constants");
+const { PORT, DB_URL } = require("./constants");
 const routes = require("./router")
 
+//Local variables
 const app = express();
 
 //Express configurations
@@ -15,6 +17,15 @@ app.use(express.urlencoded({ extended: false })); // config bodyparser
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
+
+//Database connection
+async function dbConnect() {
+    await mongoose.connect(DB_URL);
+}
+
+dbConnect()
+    .then(() => { console.log("Successfully connected to the DB") })
+    .catch((err) => { console.log("Error while connecting") })
 
 //Routes
 app.use(routes);
