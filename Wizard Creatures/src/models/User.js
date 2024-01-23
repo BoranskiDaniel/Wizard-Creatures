79569c537676331ea014039d545074e4e4aafe1b
Daniel Boranski <dboranski@gmail.com>
2024-01-23 22:12:30 +0200

    add bcrypt and hash password

--- Wizard Creatures/src/models/User.js
+++ Wizard Creatures/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
@@ -22,6 +23,11 @@ userSchema.virtual("repeatPassword").set(function (value) {
     if (value !== this.password) {
         throw new Error("Password missmatch!");
     }
+});
+
+userSchema.pre("save", async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
 })
 
 const User = mongoose.model("User", userSchema);
