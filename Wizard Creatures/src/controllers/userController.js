63144c31b8b6c4f8dd96f99e8cae8aa18889363d
Daniel Boranski <dboranski@gmail.com>
2024-01-23 22:21:57 +0200

    add login functionality

--- Wizard Creatures/src/controllers/userController.js
+++ Wizard Creatures/src/controllers/userController.js
@@ -28,11 +28,13 @@ router.post("/register", async (req, res) => {
 router.get("/login", (req, res) => {
     res.render("user/login")
 });
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
     const {
         email,
         password,
     } = req.body;
+
+    const user = await userService.login(email, password);
     res.redirect("/");
 })
 
