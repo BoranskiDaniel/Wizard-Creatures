cb96ccc910130875c23b57394aa6fe17f0754665
Daniel Boranski <dboranski@gmail.com>
2024-01-27 20:51:59 +0200

    Add create creature functionality

--- Wizard Creatures/src/controllers/postController.js
+++ Wizard Creatures/src/controllers/postController.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const creatureService = require("../services/creatureService")
 
 router.get("/all", (req, res) => {
     res.render("post/all-posts")
@@ -8,6 +9,15 @@ router.get("/create", (req, res) => {
     res.render("post/create");
 });
 
+router.post("/create", async (req, res) => {
+    const { name, species, skinColor, eyeColor, image, description } = req.body;
+
+    const payload = { name, species, skinColor, eyeColor, image, description, owner: req.user, };
+
+    await creatureService.create(payload);
+
+    res.redirect("/posts/all");
+})
 router.get("/profile", (req, res) => {
     res.render("post/profile");
 })
