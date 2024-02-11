30f5e761c25a73fe451fd482d43ff9a306c94e7d
Daniel Boranski <dboranski@gmail.com>
2024-02-11 20:18:20 +0200

    Creatures validation

--- Wizard Creatures/src/models/Creature.js
+++ Wizard Creatures/src/models/Creature.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose")
 
 
 const creatureSchema = new mongoose.Schema({
-    name: String,
-    species: String,
-    skinColor: String,
-    eyeColor: String,
-    image: String,
-    description: String,
+    name: { type: String, minLength: 2, },
+    species: { type: String, minLength: 3, },
+    skinColor: { type: String, minLength: 3, },
+    eyeColor: { type: String, minLength: 3, },
+    image: { type: String, required: true },
+    description: { type: String, minLength: 5, maxLength: 500, },
     votes: [{
         type: mongoose.Types.ObjectId,
         ref: "User",
